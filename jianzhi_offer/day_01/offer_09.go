a7279abb2f1c3a9cecbd205aa2c750461fca42c0
gopherli <twoeggsli@gmail.com>
2021-09-23 17:16:57 +0800

    剑指 Offer 09. 用两个栈实现队列

---
+++ jianzhi_offer/day_01/offer_09.go
@@ -0,0 +1,52 @@
+package day01
+
+import (
+	"fmt"
+	"time"
+)
+
+// Reference:https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+
+type CQueue struct {
+	in  stack
+	out stack
+}
+
+type stack []int
+
+func (s *stack) Push(value int) {
+	start := time.Now().Nanosecond()
+	*s = append(*s, value)
+	end := time.Now().Nanosecond()
+	fmt.Println("Push speed time => ", end-start)
+}
+
+func (s *stack) Pop() int {
+	start := time.Now().Nanosecond()
+	top := len(*s)
+	value := (*s)[0]
+	*s = (*s)[1:top]
+	end := time.Now().Nanosecond()
+	fmt.Println("Pop speed time => ", end-start)
+	return value
+}
+
+func Constructor() CQueue {
+	return CQueue{}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.in.Push(value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if len(this.out) == 0 {
+		if len(this.in) > 0 {
+			return this.in.Pop()
+		}
+	} else {
+		this.in.Pop()
+		return this.out.Pop()
+	}
+	return -1
+}
---
+++ jianzhi_offer/day_01/offer_09_test.go
@@ -0,0 +1,14 @@
+package day01
+
+import (
+	"testing"
+)
+
+func TestOffer09(t *testing.T) {
+	cq := Constructor()
+	cq.DeleteHead()
+	cq.in.Push(2)
+	cq.in.Push(3)
+	cq.DeleteHead()
+	cq.DeleteHead()
+}
